7a204ce6aab936e79460c9bda6789d5b215e712b
rimasultana <rimasultana20225@gmail.com>
2024-03-10 23:22:24 +0600

    best practic

---
+++ index.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++);
+
+let firstName = "";
+let lastName = "";
+let price = 0;
+let discount = 0;
+let fullPrice = 0,
+const myArray = [];
+const myObject = {};
+
+let x = "John";        // x is a string
+let y = new String("John");  // y is an object
+
+
+console.log(x === y)
